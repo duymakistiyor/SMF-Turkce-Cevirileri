785ef175873ad9366675c327af8d55e81e68e971
duymakistiyor <dokuz@seznam.cz>
2016-03-24 18:19:20 +0200

    Update Adk-AdminBlocks.turkish.php

--- adkportal/Adk-AdminBlocks.turkish.php
+++ adkportal/Adk-AdminBlocks.turkish.php
@@ -4,31 +4,31 @@
  * Version: 3.0
  * Official support: http://www.smfpersonal.net
  * Author: Adk Team
- * Copyright: 2009 - 2014 © SMFPersonal
+ * Copyright: 2009 - 2014 Â© SMFPersonal
  * Developers:
  * 		Juarez, Lucas Javier
  * 		Clavijo, Pablo
  *
  * version smf 2.0*
- *tükçe çeviri: http://smf.konusal.com/
+ *tÃ¼kÃ§e Ã§eviri: http://smf.konusal.com/
  */
 
 //Source File
 $txt['adkblock_templates_desc'] = 'Manage your Blocklar templates. Templates organized the Blocklar which will be shown in certain sections of your forum. The portal template is the default and can not be deleted.
 You can create multiple templates and decide which Blocklar show in the sectors that you want.';
 $txt['adkblock_first_descrip'] = 'Manage your Blocklar. Modify your position in that column wants are, edit them, delete them, etc.';
-$txt['adkblock_settings_desc'] = 'Varsýyalan block ayalarý.';
+$txt['adkblock_settings_desc'] = 'VarsÃ½yalan block ayalarÃ½.';
 $txt['adkblock_newBlocklar_desc'] = 'Add your custom block. You can choose between different types of Blocklar to create.';
-$txt['adkblock_news_desc'] = '"Haberler" için yeni blok ekle';
-$txt['adkblock_topposter'] = 'En çok Yazanlar';
+$txt['adkblock_news_desc'] = '"Haberler" iÃ§in yeni blok ekle';
+$txt['adkblock_topposter'] = 'En Ã§ok Yazanlar';
 $txt['adkblock_php'] = 'Php';
 $txt['adkblock_bbc'] = 'BBCode';
 $txt['adkblock_auto_news'] = 'Automatic News';
 $txt['adkblock_top_karma'] = 'Top Karma';
 $txt['adkblock_staff'] = 'Staff';
-$txt['adkblock_multi_block'] = 'Çoklu Blok';
-$txt['adkblock_create_custom_block'] = 'Özgür blok oluþtur';
-$txt['adkblock_none'] = 'None';
+$txt['adkblock_multi_block'] = 'Ã‡oklu Blok';
+$txt['adkblock_create_custom_block'] = 'Ã–zgÃ¼r blok oluÃ¾tur';
+$txt['adkblock_none'] = 'HiÃ§biri';
 $txt['adkblock_permissions'] = 'Permissions';
 $txt['adkblock_download_personal_desc'] = 'Install Blocklar directly from SMF Personal. To install one of these Blocklar, click on the Install button and Adkportal will do the rest.';
 
@@ -37,58 +37,58 @@ $txt['adkblock_aporte'] = 'Block: Haberleri';
 $txt['adkblock_shout'] = 'Block: SohbetKutusu';
 $txt['adkblock_number_news'] = 'Number of news per page.';
 $txt['adkblock_twitter_facebook'] = 'Facebook vs Twiiter yerimlerini block ekle';
-$txt['adkblock_top_poster'] = 'Block: En çok Yazanlar';
-$txt['adkblock_top_poster_limit'] = 'User Limit in En çok Yazanlar.';
+$txt['adkblock_top_poster'] = 'Block: En Ã§ok Yazanlar';
+$txt['adkblock_top_poster_limit'] = 'User Limit in En Ã§ok Yazanlar.';
 $txt['adkblock_number_post'] = 'Number of topics to display';
 $txt['adkblock_last_post'] = 'Block: Son Konular';
-$txt['adkblock_users_online'] = 'Block: Açýk Üyeler';
+$txt['adkblock_users_online'] = 'Block: AÃ§Ã½k Ãœyeler';
 $txt['adkblock_install'] = 'Install';
 $txt['adkblock_go_to_personal'] = 'SMF Personal git';
 $txt['adkblock_vertically_who'] = 'Show vertically.';
 $txt['adkblock_autonews'] = 'Block: Otomatik Haberler';
-$txt['adkblock_avatar_topposter'] = 'Avatarlarý kapat';
-$txt['adkblock_two_column'] = 'Çift Kolon aktif et';
+$txt['adkblock_avatar_topposter'] = 'AvatarlarÃ½ kapat';
+$txt['adkblock_two_column'] = 'Ã‡ift Kolon aktif et';
 $txt['adkblock_news_limit_topics'] = 'Limit of topics to showing in the block';
 $txt['adkblock_auto_news_limit_body'] = 'Limit of Characters';
 $txt['adkblock_disable_autor'] = 'Disable who added the news';
-$txt['adkblock_news_size_img'] = 'Resimlerin enini sýnýrla';
+$txt['adkblock_news_size_img'] = 'Resimlerin enini sÃ½nÃ½rla';
 $txt['adkblock_autonews_boards'] = 'Boards';
 $txt['adkblock_titulo'] = 'Title';
-$txt['adkblock_editing_block'] = 'Block düzenle';
+$txt['adkblock_editing_block'] = 'Block dÃ¼zenle';
 $txt['adkblock_editing_block_code'] = 'Code';
 $txt['adkblock_options'] = 'Options';
-$txt['adkblock_empty_style_block'] = 'Site kalýbý css';
-$txt['adkblock_empty_title'] = 'Baþlýk gösterme';
-$txt['adkblock_empty_collapse'] = 'Kapatmayý kaldýr';
+$txt['adkblock_empty_style_block'] = 'Site kalÃ½bÃ½ css';
+$txt['adkblock_empty_title'] = 'BaÃ¾lÃ½k gÃ¶sterme';
+$txt['adkblock_empty_collapse'] = 'KapatmayÃ½ kaldÃ½r';
 $txt['adkblock_icons'] = 'Icon';
 $txt['adkblock_include'] = 'Included';
-$txt['adkblock_select_create'] = 'Block oluþtur';
-$txt['adkblock_select_tipe'] = 'Block formatýný seç';
+$txt['adkblock_select_create'] = 'Block oluÃ¾tur';
+$txt['adkblock_select_tipe'] = 'Block formatÃ½nÃ½ seÃ§';
 $txt['adkblock_select_insert'] = '------ Blocklar ------';
 $txt['adkblock_insert'] = 'Block eklendi';
-$txt['adkblock_multi_block_desc'] = 'You can place on this block, some Blocklar, allowing customize your portal. Remember that you can only Block seçildi already created in this section.';
-$txt['adkblock_multi_block_select'] = 'Block seçildi';
-$txt['adkblock_multi_block_selected'] = 'Block seçildi';
-$txt['adkblock_top'] = 'Üst';
+$txt['adkblock_multi_block_desc'] = 'You can place on this block, some Blocklar, allowing customize your portal. Remember that you can only Block seÃ§ildi already created in this section.';
+$txt['adkblock_multi_block_select'] = 'Block seÃ§ildi';
+$txt['adkblock_multi_block_selected'] = 'Block seÃ§ildi';
+$txt['adkblock_top'] = 'Ãœst';
 $txt['adkblock_bottom'] = 'Alt';
 $txt['adkblock_left'] = 'Sol';
 $txt['adkblock_center'] = 'Orta';
-$txt['adkblock_right'] = 'Sað';
+$txt['adkblock_right'] = 'SaÃ°';
 $txt['adkblock_admin'] = 'Unused Blocklar';
 $txt['adkblock_none'] = 'Not used';
 $txt['adkblock_autor'] = 'Yapan';
-$txt['adkblock_upload_yourBlock_info'] = 'PHP block seç';
-$txt['adkblock_upload'] = 'Dosyayý seç';
+$txt['adkblock_upload_yourBlock_info'] = 'PHP block seÃ§';
+$txt['adkblock_upload'] = 'DosyayÃ½ seÃ§';
 $txt['adkblock_shout_g_allowed_view'] = 'Allowed groups to see Shoutbox';
 $txt['adkblock_shout_g_allowed_topost'] = 'Allowed groups to post on Shoutbox';
-$txt['adkblock_delete_allshoutbox'] = 'Sohbet kutusundaki mesajlarý temizle';
+$txt['adkblock_delete_allshoutbox'] = 'Sohbet kutusundaki mesajlarÃ½ temizle';
 $txt['adkblock_guests'] = 'Guest';
-$txt['adkblock_regulars_users'] = 'Düzenli kullanýcýlar';
-$txt['adkblock_select_group'] = 'Gruplarý seç';
-$txt['adkblock_preview'] = 'Önceki block';
+$txt['adkblock_regulars_users'] = 'DÃ¼zenli kullanÃ½cÃ½lar';
+$txt['adkblock_select_group'] = 'GruplarÃ½ seÃ§';
+$txt['adkblock_preview'] = 'Ã–nceki block';
 $txt['adkblock_permissions_desc'] = 'Here you can select which groups will not see this block';
 $txt['adkblock_editing_permissions'] = 'Editing permissions';
-$txt['adkblock_check_all'] = 'Alayýný seç';
+$txt['adkblock_check_all'] = 'AlayÃ½nÃ½ seÃ§';
 $txt['adkblock_limit'] = 'Amount of users displayed';
 $txt['adkblock_select_forums'] = 'Select the boards that will be shown on the block';
 $txt['adkblock_creating'] = 'Block ekle';
@@ -96,8 +96,8 @@ $txt['adkblock_edit'] = 'Edit';
 $txt['adkblock_pre_view'] = 'Preview';
 $txt['adkblock_delete_waring'] = 'Want to delete this block? This operation can not returned.';
 $txt['adkblock_delete'] = 'Sil';
-$txt['adkblock_body'] = 'Vücut';
-$txt['adkblock_avatar'] = 'Avatarý göster';
+$txt['adkblock_body'] = 'VÃ¼cut';
+$txt['adkblock_avatar'] = 'AvatarÃ½ gÃ¶ster';
 $txt['adkblock_limit_auto_news'] = 'Amount of News per Page';
 $txt['adkblock_template_action'] = 'Aktiviteler';
 $txt['adkblock_template_default'] = 'Portal';
@@ -128,6 +128,6 @@ $txt['adkblock_top_poster_help'] = 'Create a new top poster block with new setti
 $txt['adkblock_auto_news_help'] = 'Create another automatic news block';
 $txt['adkblock_top_karma_help'] = 'Create a top karma block';
 $txt['adkblock_staff_help'] = 'Create a custom staff block with your favourites groups ;)';
-$txt['adkblock_multi_block_help'] = 'Create a new block with Çoklu Blok';
+$txt['adkblock_multi_block_help'] = 'Create a new block with Ã‡oklu Blok';
 
 ?>
